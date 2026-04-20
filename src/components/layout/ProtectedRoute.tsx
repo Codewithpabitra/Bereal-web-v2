5fc173f57082d61f9ea8bcbca3c9037071f98195
Codewithpabitra <pabitra05.maity@gmail.com>
2026-04-20 17:14:55 +0530

    fix few type typos

--- src/components/layout/ProtectedRoute.tsx
+++ src/components/layout/ProtectedRoute.tsx
@@ -1,7 +1,8 @@
+import React from 'react'
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 
-export const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+export const ProtectedRoute = ({ children }: { children: React.ReactElement }) => {
   const { user, loading } = useAuth();
   if (loading) return null;
   return user ? children : <Navigate to="/login" />;
